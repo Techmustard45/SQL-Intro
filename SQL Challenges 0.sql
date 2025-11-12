b82d1fd2c28366b0b00376007035f9a16a04ec97
Elias <elias.lavrich@gmail.com>
2025-11-12 10:44:32 -0500

    Fixed spelling errors

--- SQL Challenges 0.sql
+++ SQL Challenges 0.sql
@@ -9,7 +9,7 @@ SELECT * From bestbuy.products WHERE NOT products.Price = 11.99;
 -- find all products and sort them by price from greatest to least
 Select * FROM bestbuy.products ORDER BY products.price DESC;
 -- find all employees who don't have a middle initial
-SELECT * FROM bestbuy.employees WHERE MiddleInital IS NULL;
+SELECT * FROM bestbuy.employees WHERE MiddleInitial IS NULL;
 -- find distinct product prices
 SELECT DISTINCT bestbuy.products.price FROM bestbuy.products;
 -- find all employees whose first name starts with the letter ‘j’
@@ -19,7 +19,7 @@ SELECT * FROM bestbuy.products WHERE NAME = "Mackbook";
 -- find all products that are on sale
 SELECT * FROM bestbuy.products WHERE OnSale = 1;
 -- find the average price of all products
-SELECT AVG(prducts.price) FROM bestbuy.products;
+SELECT AVG(products.price) FROM bestbuy.products;
 -- find all Geek Squad employees who don't have a middle initial
 SELECT * FROM bestbuy.employees WHERE employees.MiddleInitial IS NULL AND employees.title = 'Geek Squad';
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
